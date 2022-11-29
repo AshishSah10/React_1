db872a7e37d7f24180e2a6111f44d910737acd57
Ashish Sah <ashish0010sah@gmail.com>
2022-11-30 02:13:10 +0530

    callback, useState, useEffect, fetch, promise
    Signed-off-by: Ashish Sah <ashish0010sah@gmail.com>

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useEffect, useState, version } from "react";
 //import ProductCard from "../ProductCard/ProductCard";
 
 import ProductCard from "../ProductCard";
 
-const products = [
+const _products = [
     {
         title: "IPhone 14 pro",
         price: "Rs 1,00,000"
@@ -21,18 +21,101 @@ const products = [
         price: "Rs 10,000"
     },
 ]
+
+function getProducts(callback){
+    console.log("api called")
+    setTimeout(() => callback(_products), 3000);
+}
+
 function ProductList(){
-    return (
-        <>
-        <div>Product List</div>
-        {
-            products.map(
-                product => (<ProductCard title = {product.title} price = {product.price}/>),
-                {/* product => <ProductCard {...product}/> */}
-            )
-        }
-        </>
-    )
+    const [isError, setError] = useState(null);
+
+    let isLoadingState = useState(true);
+    // [isLoading, setLoading() ]
+    // const [isLoading, setLoading] = useState(true);
+    let isLoading = isLoadingState[0];
+    let setIsLoading = isLoadingState[1];
+    
+    let productState = useState([]);
+    let products = productState[0];
+    let setProducts = productState[1];
+
+    
+    console.log("Api call started ", isLoading, products);
+    // getProducts(function(results){
+    //     // isLoading = false;
+    //     // products = results;
+    //     setIsLoading(false);
+    //     setProducts(results);
+    //     console.log("Api call ended ", isLoading, products);
+    // });
+
+    // mounting phase
+    // useEffect(() => {
+    //     getProducts(function(results){
+    //         // isLoading = false;
+    //         // products = results;
+    //         setIsLoading(false);
+    //         setProducts(results);
+    //         console.log("Api call ended ", isLoading, products);
+    //     });
+    // }, []);
+
+    // mounting phase
+    // useEffect(() => {
+    //     const promise = fetch("http://localhost:3001/products");
+    //     promise.then((response) => {
+    //         console.log("fetching successful ", response);
+    //         const promise1 = response.json()
+    //         promise1.then((result) => {
+    //             console.log("parsing json successful ",result);
+    //         });
+    //         promise1.catch((error) => {
+    //             console.log("error parsing json ", error);
+    //         })
+    //     });
+    //     promise.catch((error) => {
+    //         console.log("fetching failed ", error);
+    //     })
+    // }, []);
+
+    // mounting phase
+    useEffect(() => {
+        fetch("http://localhost:3001/products")
+            .then((response) => {
+                console.log("fetching successful ", response);
+                return response.json();
+            })
+            .then((result) => {
+                console.log("parsing json successful ", result);
+                setIsLoading(false);
+                setProducts(result);
+            })
+            .catch(error => {
+                console.log("error in fetch/parse ", error);
+                setError(error);
+            })
+    }, []);
+
+    if(isError){
+        return <h5>Somthing is Wrong!!</h5>;
+    }
+    else if(isLoading){
+        return <h3>Loading...</h3>;
+    }
+    else{
+        return (
+            <>
+                <div>Product List</div>
+                {
+                    products.map(
+                        product => (<ProductCard title = {product.title} price = {product.price}/>),
+                        {/* product => <ProductCard {...product}/> */}
+                    )
+                }
+            </>
+        )
+    }
 }
 
 export default ProductList;
